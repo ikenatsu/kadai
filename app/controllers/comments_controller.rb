8e6e85f40446282ced7cd56c4f8039dbe12c4049
ikenatsu <kei.trmc.s2-2sc@outlook.jp>
2024-02-09 23:34:16 +0900

    コメント送信機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,20 @@ class CommentsController < ApplicationController
     @comment = Comment.new
     @problem = Problem.find(params[:problem_id])
   end
+
+  def create
+    @problem = Problem.find(params[:problem_id])
+    @comment = @problem.comments.new(comment_params)
+    if @comment.save
+      redirect_to problem_comments_path(@problem)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
+  end
 end
