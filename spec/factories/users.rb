891729c97c6f71f91f1d20b1af3a80b29a11d82c
ikenatsu <kei.trmc.s2-2sc@outlook.jp>
2024-02-23 23:52:13 +0900

    ユーザーのファクトリーボット

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :user do
-    name {Faker::Name.last_name}
+    nickname {Faker::Name.last_name}
     email {Faker::Internet.email}
     password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    gender_id { rand(1..3) }
+    age {rand(1..120) }
+    occupation_id { rand(1..15) }
+    family_id { rand(1..5) }
   end
 end
